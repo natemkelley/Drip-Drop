9e5dc392b82d511023f8a3bbb4832dc99daeafad
Nate <natemkelley@gmail.com>
2018-10-24 23:23:29 -0600

    dummy data

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,6 +1,7 @@
 //establish database connection
 var firebase = require('firebase');
 var compute = require('./compute.js');
+var fs = require('fs');
 
 var config = {
     apiKey: "AIzaSyDJIPWAjanHdO9WBdmb5cBhaRjSVbNdpYk",
@@ -12,13 +13,150 @@ var config = {
 };
 firebase.initializeApp(config);
 
-
 var rootRef = firebase.database().ref();
 rootRef.on("value", function (snapshot) {
-    console.log(snapshot.val());
+    if (snapshot.val() == null) {
+        massInputDummyData();
+    } else {
+
+    }
 })
 
 
 exports.test = function () {
     return 'yasss'
+}
+
+var XXX = 0;
+var success = 0;
+var tried = 0;
+
+function dummyData() {
+    XXX++;
+    console.log(XXX)
+    var jsondata = {};
+    var year = 2018;
+
+    for (month = 0; month < 11; month++) {
+        for (dayx = 0; dayx < 2; dayx++) {
+            for (hourx = 0; hourx < 2; hourx++) {
+                var hourRun = runOnHour();
+                for (minutex = 0; minutex < 2; minutex++) {
+                    var minuteRun = runOnMinute();
+                    for (second = 0; second < 2; second++) {
+                        var d = new Date(year, month, day, hour, minute, second, 0);
+                        var liters = 0.00;
+                        if (minuteRun && hourRun) {
+                            liters = randomLiterage();
+                        }
+                        var data = {
+                            liters: liters,
+                            timestamp: Date.parse(d)
+                        }
+                        var day = randomDay();
+                        var minute = randomMinute();
+                        var hour = randomHour();
+                        var stringDot = createStringDot(year, month, day, hour, minute, second)
+                        setDat(stringDot, data)
+
+                        writeDummyData(stringDot, data)
+                    }
+                }
+            }
+        }
+    }
+
+    function setDat(path, value) {
+        var schema = jsondata;
+        var pList = path.split('.');
+        var len = pList.length;
+        for (var i = 0; i < len - 1; i++) {
+            var elem = pList[i];
+            if (!schema[elem]) schema[elem] = {}
+            schema = schema[elem];
+        }
+
+        schema[pList[len - 1]] = value;
+    }
+
+    function createStringDot(year, month, day, hour, minute, second) {
+        var stringDot = year.toString();
+        stringDot += "." + month.toString();
+        stringDot += "." + day.toString();
+        stringDot += "." + hour.toString();
+        stringDot += "." + minute.toString();
+        stringDot += "." + second.toString();
+        return stringDot
+    }
+
+    function randomLiterage() {
+        let maxLiterage = 2.65;
+        let precision = 1000;
+        var returnVal = 0;
+        returnVal = Math.floor(Math.random() * (maxLiterage * precision - 1 * precision) + 1 * precision) / (1 * precision);
+
+        return returnVal
+    }
+
+    function runOnHour() {
+        var prob = Math.floor(Math.random() * 10);
+        var returnVal = false;
+        if (prob < 9) {
+            returnVal = true;
+        }
+        return returnVal
+    }
+
+    function runOnMinute() {
+        var rando = Math.floor(Math.random() * 2);
+        var returnVal = false;
+        if (rando == 1) {
+            returnVal = true
+        }
+        return returnVal
+    }
+
+    function randomDay() {
+        var rando = Math.floor(Math.random() * 30);
+        return rando
+
+    }
+
+    function randomMinute() {
+        var rando = Math.floor(Math.random() * 59);
+        return rando
+    }
+
+    function randomHour() {
+        var rando = Math.floor(Math.random() * 24);
+        return rando
+    }
+
+    function writeDummyData(stringDot, data) {
+        var pathSave = stringDot.replace(/\./g, "/");
+        var randoPipe = Math.floor(Math.random() * 2);
+        var pipe = "pipe" + randoPipe.toString();
+        var pathSave = pipe + "/" + pathSave;
+        tried++;
+
+        if (data.timestamp > 10) {
+            firebase.database().ref(pathSave).set(data, function (error) {
+                if (error) {
+                    console.log(error)
+                } else {
+                    success++;
+                    console.log("success=" + success);
+                    console.log('tried=' + tried)
+                }
+            });
+        }
+    }
+}
+
+function massInputDummyData() {    
+    for (xxx = 0; xxx < 15; xxx++) {
+        setTimeout(function () {
+            dummyData()
+        }, 5000 * xxx);
+    }
 }
