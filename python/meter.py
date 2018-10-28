b969f2459f254a381fcb0532c27429b1a4a00acd
Nate <natemkelley@gmail.com>
2018-10-27 18:36:02 -0600

    swag

--- python/meter.py
+++ python/meter.py
@@ -13,29 +13,22 @@ time_new = 0.0
 dunzo = True
 
 while dunzo:
-	time_new = time.time() + 5 #+60 = 1 minute
-	rate_cnt = 0
-	
-	while time.time() <= time_new:
-		try:
-			if GPIO.input(inpt) !=0:
-				rate_cnt += 1
-
-#print to nodejs
-    dunzo = false
-	data = {}
-	data['status'] = '200'
-	data['usage'] = round(rate_cnt * constant,4)
-	data['cycle'] = minutes
-	
-	if(data['usage']>11):
-		data['status'] = '500'	
-	  
-	json_data = json.dumps(data)
-	print(json_data)
-	sys.stdout.flush()
+    time_new = time.time() + 5
+    rate_cnt = 0
+    while time.time() <= time_new:
+        if GPIO.input(inpt)!=0:
+            rate_cnt+=1
+    
+    dunzo = False
+    data = {}
+    data['status'] = '200'
+    data['usage'] = round(rate_cnt * constant,4)
+    data['cycle'] = minutes
+    
+    if(data['usage']>11):
+        data['status']='500'
+    json_data = json.dumps(data)
+    print(json_data)
+    sys.stdout.flush()
     GPIO.cleanup()
     sys.exit()
-
-	
-	
